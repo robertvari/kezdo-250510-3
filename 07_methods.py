3f9828e23789e47002b99380169f160327500fad
Robert Vari <mail.robertvari@gmail.com>
2025-06-07 15:36:26 +0200

    update

--- 07_methods.py
+++ 07_methods.py
@@ -1,4 +1,4 @@
-import time, random
+import time, random, os
 
 class CoffeeMaker:
     coffee_types = ["Espresso", "Doppio", "Latte", "Cappuccino"]
@@ -7,12 +7,17 @@ class CoffeeMaker:
         self.model = model
         self.water = 100
         self.beans = 100
-        self.cofees_made = 0
+        self.coffees_made = 0
     
     def display(self):
-        print(f"Model: {self.model}, Water: {self.water}, Beans: {self.beans}, Coffees Made: {self.cofees_made}")
+        print(f"Model: {self.model}, Water: {self.water}, Beans: {self.beans}, Coffees Made: {self.coffees_made}")
 
     def make_coffee(self):
+        self.coffees_made += 1
+        self.water -= random.randint(20, 50)
+        self.beans -= random.randint(10, 30)
+
+        os.system("cls")
         print("-"*10, self.model, "-"*10)
         for index, i in enumerate(self.coffee_types):
             print(index, i)
@@ -20,9 +25,19 @@ class CoffeeMaker:
         coffe_number = int(input("Choose your coffee: "))
 
         print(f"Making a {self.coffee_types[coffe_number]}...")
-        time.sleep(random.randint(3,6))
-        print(f"Your {self.coffee_types[coffe_number]} is ready")
-        print("Enjoy")
+        time.sleep(random.randint(1,3))
+        print(f"Your {self.coffee_types[coffe_number]} is ready. Enjoy :)")
 
 my_coffe_maker = CoffeeMaker("Sencor")
 my_coffe_maker.make_coffee()
+my_coffe_maker.display()
+time.sleep(3)
+
+my_coffe_maker.make_coffee()
+my_coffe_maker.display()
+time.sleep(3)
+
+my_coffe_maker.make_coffee()
+my_coffe_maker.display()
+time.sleep(3)
+
