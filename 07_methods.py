f78d29ee950006e445668bdcf46e349447500a31
Robert Vari <mail.robertvari@gmail.com>
2025-06-07 15:26:18 +0200

    update

--- 07_methods.py
+++ 07_methods.py
@@ -1,3 +1,5 @@
+import time, random
+
 class CoffeeMaker:
     coffee_types = ["Espresso", "Doppio", "Latte", "Cappuccino"]
     
@@ -10,6 +12,17 @@ class CoffeeMaker:
     def display(self):
         print(f"Model: {self.model}, Water: {self.water}, Beans: {self.beans}, Coffees Made: {self.cofees_made}")
 
+    def make_coffee(self):
+        print("-"*10, self.model, "-"*10)
+        for index, i in enumerate(self.coffee_types):
+            print(index, i)
+
+        coffe_number = int(input("Choose your coffee: "))
+
+        print(f"Making a {self.coffee_types[coffe_number]}...")
+        time.sleep(random.randint(3,6))
+        print(f"Your {self.coffee_types[coffe_number]} is ready")
+        print("Enjoy")
 
 my_coffe_maker = CoffeeMaker("Sencor")
-my_coffe_maker.display()
+my_coffe_maker.make_coffee()
