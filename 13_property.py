c5545222d7ea12ea9f60d41d9eea9ec07ac4589b
Robert Vari <mail.robertvari@gmail.com>
2025-06-07 17:17:01 +0200

    update

--- 13_property.py
+++ 13_property.py
@@ -21,7 +21,22 @@ class CoffeeMaker:
     def model(self):
         return self.__model
 
-philips = CoffeeMaker("PHILIPS HD7411", "White", 12999)
-print(philips.price)
-philips.price = 1000
-print(philips.price)
+class Customer:
+    def __init__(self, first_name, last_name):
+        self.__first_name = first_name
+        self.__last_name = last_name
+    
+    @property
+    def first_name(self):
+        return self.__first_name
+    
+    @property
+    def last_name(self):
+        return self.__last_name
+    
+    @property
+    def full_name(self):
+        return f"{self.__first_name} {self.__last_name}"
+
+csaba = Customer("Kiss", "Csaba")
+print(csaba.full_name)
