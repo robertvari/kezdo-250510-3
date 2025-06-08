f11ece0d73a4d9ae09d39fd2de7043bebf50aa06
Robert Vari <mail.robertvari@gmail.com>
2025-06-08 15:19:34 +0200

    update

---
+++ Blackjack/Blackjack.py
@@ -0,0 +1,14 @@
+class Blackjack:
+    def __init__(self):
+        self.intro()
+
+    def intro(self):
+        pass
+
+    def exit_game(self):
+        pass
+
+    def clear_screen(self):
+        pass
+
+Blackjack()
