24d7a35c2d9911dd632c0318d76a67c0eb8bd067
Robert Vari <mail.robertvari@gmail.com>
2025-06-08 15:47:53 +0200

    update

--- Blackjack/game_assets/cards.py
+++ Blackjack/game_assets/cards.py
@@ -18,15 +18,19 @@ class Card:
         return self.__name
 
 class Deck:
-    pass
+    def __init__(self):
+        self.__cards = []
+        self.create()
+    
+    def create(self):
+        pass
+    
+    def __str__(self):
+        return str(self.__cards)
 
 
 if __name__ == "__main__":
     suits = ["Heart ♥", "Club ♣", "Diamond ♦", "Spade ♠"]
 
-    card1 = Card("♠ Ace", 11)
-    card2 = Card("♣ King", 10)
-    card3 = Card("♦ Queen", 10)
-    
-    deck = [card1, card2, card3]
+    deck = Deck()
     print(deck)
