5f0fda4dc7a6989f697e085e2734122917aabf84
Robert Vari <mail.robertvari@gmail.com>
2025-06-08 14:37:04 +0200

    update

---
+++ 14_inheritance.py
@@ -0,0 +1,29 @@
+import random
+
+class Player_BASE:
+    def __init__(self):
+        self.my_number = 0
+        self.name = None
+
+    def think_number(self):
+        self.my_number = random.randint(1, 10)
+
+
+class Player(Player_BASE):
+    # partial method override
+    def __init__(self):
+        # runs BASE class __init__()
+        super().__init__()
+        self.name = "Csaba"
+    
+    # full method override
+    def think_number(self):
+        self.my_number = int(input("What is your guess? "))
+
+class Computer(Player_BASE):
+    pass
+
+player = Player()
+computer = Computer()
+
+player.think_number()
