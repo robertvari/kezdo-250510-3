a66e4879f4210dc8ab5d02da7363887fb66422b6
Robert Vari <mail.robertvari@gmail.com>
2025-06-08 15:49:37 +0200

    update

--- Blackjack/game_assets/cards.py
+++ Blackjack/game_assets/cards.py
@@ -24,6 +24,21 @@ class Deck:
     
     def create(self):
         suits = ["♥", "♣", "♦", "♠"]
+        cards = [
+            ["2", 2],
+            ["3", 3],
+            ["4", 4],
+            ["5", 5],
+            ["6", 6],
+            ["7", 7],
+            ["8", 8],
+            ["9", 9],
+            ["10", 10],
+            ["King", 10],
+            ["Queen", 10],
+            ["Jack", 10],
+            ["Ace", 11]
+        ]
     
     def __str__(self):
         return str(self.__cards)
